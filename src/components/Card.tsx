b3f58c2457e7cece93a28c9a2e0fb71c676f9929
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-08-06 15:20:08 -0400

    adjust size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -46,10 +46,10 @@ export default function Card() {
               </span>
             </div>
             <div className="flex items-center justify-between">
-              <span className="text-2xl font-bold text-gray-900 dark:text-white">{product.price}</span>
+              <span className="text-xl font-bold text-gray-900 dark:text-white">{product.price}</span>
               <a
                 href="#"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Add to cart
               </a>
